59909fd0e275e8db2d3fb3721e0d3e4385ef4449
Christian Buus Michelsen <christianmichelsen@gmail.com>
2025-05-15 23:02:08 +0200

    refactor

--- _main.py
+++ _main.py
@@ -5,7 +5,8 @@ import json
 import requests
 from rich import print
 
-from src.bolig_ping.data_models import Home, SearchQuery
+from bolig_ping.data_models import Home, SearchQuery
+from bolig_ping.rejseplanen import Journey, Location, TripRequest
 
 # %%
 
@@ -67,3 +68,27 @@ print(home.to_text())
 
 
 # polygon = "12.467721,55.768600|12.482656,55.759274|12.506124,55.758190|12.519962,55.767550|12.510944,55.778394|12.509179,55.788153|12.506264,55.797984|12.481980,55.806335|12.474718,55.817548|12.453262,55.816325|12.433050,55.812394|12.429428,55.811859|12.423876,55.811171|12.415170,55.800073|12.423254,55.788798|12.427297,55.776908|12.446732,55.771573|12.467721,55.768600"  # noqa: E501
+
+
+# %%
+
+
+origin_location = Location.from_string("Frugthegnet 42, 2830 Virum")
+
+request = TripRequest(
+    originCoordLat=origin_location.lat,
+    originCoordLong=origin_location.lon,
+    destId="8600646",  # (Nørreport st)
+    # destCoordLat="55.683597",  # Destination latitude
+    # destCoordLong="12.5708992",  # Destination longitude
+    date="2025-05-19",  # Monday's date in YYYY-MM-DD format
+    time="08:00",  # Time in hh:mm format
+)
+response = request.get_response()
+
+
+journey = Journey(**response)
+trip1 = journey.get_fastest_trip_simple()
+trip2 = journey.get_fastest_trip_corrected()
+print(trip1.description)
+print(trip2.description)
